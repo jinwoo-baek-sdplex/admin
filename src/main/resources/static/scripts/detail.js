1f1b86849ac49e7494e9f537e729b1be8850b550
Jinwoo-Home <back3946@gmail.com>
2020-09-24 02:55:37 +0900

    토글에 id 바꿔서 시도해보기

--- src/main/resources/static/scripts/detail.js
+++ src/main/resources/static/scripts/detail.js
@@ -1,175 +1,186 @@
 // About Chart
-$(function () {
-    /*
-     * Flot Interactive Chart
-     * -----------------------
-     */
-    // We use an inline data source in the example, usually data would
-    // be fetched from a server
-    var data = [],
-        totalPoints = 100
 
-    function getRandomData() {
+var data = [],
+    totalPoints = 100
 
-        if (data.length > 0) {
-            data = data.slice(1)
-        }
+function getRandomData() {
 
-        // Do a random walk
-        while (data.length < totalPoints) {
-
-            var prev = data.length > 0 ? data[data.length - 1] : 50,
-                y = prev + Math.random() * 10 - 5
-
-            if (y < 0) {
-                y = 0
-            } else if (y > 100) {
-                y = 100
-            }
-
-            data.push(y)
-        }
-
-        // Zip the generated y values with the x values
-        var res = []
-        for (var i = 0; i < data.length; ++i) {
-            res.push([i, data[i]])
-        }
-
-        return res
+    if (data.length > 0) {
+        data = data.slice(1)
     }
 
-    var cpu_chart = $.plot('#cpu-chart', [
-            {
-                data: getRandomData(),
-            }
-        ],
-        {
-            grid: {
-                borderColor: '#f3f3f3',
-                borderWidth: 1,
-                tickColor: '#f3f3f3'
-            },
-            series: {
-                color: '#3c8dbc',
-                lines: {
-                    lineWidth: 2,
-                    show: true,
-                    fill: true,
-                },
-            },
-            yaxis: {
-                min: 0,
-                max: 100,
-                show: true
-            },
-            xaxis: {
-                show: true
-            }
+    // Do a random walk
+    while (data.length < totalPoints) {
+
+        var prev = data.length > 0 ? data[data.length - 1] : 50,
+            y = prev + Math.random() * 10 - 5
+
+        if (y < 0) {
+            y = 0
+        } else if (y > 100) {
+            y = 100
         }
-    )
 
-    var memory_chart = $.plot('#memory-chart', [
-            {
-                data: getRandomData(),
-            }
-        ],
-        {
-            grid: {
-                borderColor: '#f3f3f3',
-                borderWidth: 1,
-                tickColor: '#f3f3f3'
-            },
-            series: {
-                color: '#BA55D3',
-                lines: {
-                    lineWidth: 2,
-                    show: true,
-                    fill: true,
-                },
-            },
-            yaxis: {
-                min: 0,
-                max: 100,
-                show: true
-            },
-            xaxis: {
-                show: true
-            }
-        }
-    )
-
-    var updateInterval = 1000 //Fetch data ever x milliseconds
-
-    function cpuResourceUpdate() {
-        cpu_chart.setData([getRandomData()])
-        cpu_chart.draw()
-        setTimeout(cpuResourceUpdate, updateInterval)
+        data.push(y)
     }
 
-    function memoryResourceUpdate() {
-        memory_chart.setData([getRandomData()])
-        memory_chart.draw()
-        setTimeout(memoryResourceUpdate, updateInterval)
+    // Zip the generated y values with the x values
+    var res = []
+    for (var i = 0; i < data.length; ++i) {
+        res.push([i, data[i]])
     }
 
+    return res
+}
 
-    // //INITIALIZE REALTIME DATA FETCHING
-    // // about performance
-    // processingSpeedUpdate()
-    // processingDataUpdate()
-
-    // about resource usage
-    cpuResourceUpdate()
-    memoryResourceUpdate()
-
-
-    /*
-     * DONUT CHART
-     * -----------
-     */
-
-    var donutData = [
+var processingSpeed_chart = $.plot('#processingSpeed-chart', [
         {
-            label: 'Usage',
-            data : 30,
-            color: '#3c8dbc'
+            data: getRandomData(),
+        }
+    ],
+    {
+        grid: {
+            borderColor: '#f3f3f3',
+            borderWidth: 1,
+            tickColor: '#f3f3f3'
         },
-        {
-            label: 'Empty',
-            data : 70,
-            color: '#00c0ef'
-        }
-    ]
-    $.plot('#donut-chart', donutData, {
         series: {
-            pie: {
-                show       : true,
-                radius     : 1,
-                innerRadius: 0.5,
-                label      : {
-                    show     : true,
-                    radius   : 2 / 3,
-                    formatter: labelFormatter,
-                    threshold: 0.1
-                }
-
-            }
+            color: '#3c8dbc',
+            lines: {
+                lineWidth: 2,
+                show: true,
+                fill: true,
+            },
         },
-        legend: {
-            show: false
+        yaxis: {
+            min: 0,
+            max: 100,
+            show: true
+        },
+        xaxis: {
+            show: true
         }
-    })
-    /*
-     * END DONUT CHART
-     */
-})
+    }
+)
+
+var processingData_chart = $.plot('#processingData-chart', [
+        {
+            data: getRandomData(),
+        }
+    ],
+    {
+        grid: {
+            borderColor: '#f3f3f3',
+            borderWidth: 1,
+            tickColor: '#f3f3f3'
+        },
+        series: {
+            color: '#3c8dbc',
+            lines: {
+                lineWidth: 2,
+                show: true,
+                fill: true,
+            },
+        },
+        yaxis: {
+            min: 0,
+            max: 100,
+            show: true
+        },
+        xaxis: {
+            show: true
+        }
+    }
+)
+
+var memory_chart = $.plot('#memory-chart', [
+        {
+            data: getRandomData(),
+        }
+    ],
+    {
+        grid: {
+            borderColor: '#f3f3f3',
+            borderWidth: 1,
+            tickColor: '#f3f3f3'
+        },
+        series: {
+            color: '#BA55D3',
+            lines: {
+                lineWidth: 2,
+                show: true,
+                fill: true,
+            },
+        },
+        yaxis: {
+            min: 0,
+            max: 100,
+            show: true
+        },
+        xaxis: {
+            show: true
+        }
+    }
+)
+
+// var donutData = [
+//     {
+//         label: 'Usage',
+//         data: 30,
+//         color: '#3c8dbc'
+//     },
+//     {
+//         label: 'Empty',
+//         data: 70,
+//         color: '#00c0ef'
+//     }
+// ]
+// var disk_chart = $.plot('#disk-chart', donutData, {
+//     series: {
+//         pie: {
+//             show: true,
+//             radius: 1,
+//             innerRadius: 0.5,
+//             label: {
+//                 show: true,
+//                 radius: 2 / 3,
+//                 formatter: labelFormatter,
+//                 threshold: 0.1
+//             }
+//
+//         }
+//     },
+//     legend: {
+//         show: false
+//     }
+// })
+
+var updateInterval = 1000 //Fetch data ever x milliseconds
+
+function processingSpeedUpdate() {
+    processingSpeed_chart.setData([getRandomData()])
+    processingSpeed_chart.draw()
+    setTimeout(processingSpeedUpdate, updateInterval)
+}
+
+function processingDataUpdate() {
+    processingData_chart.setData([getRandomData()])
+    processingData_chart.draw()
+    setTimeout(processingDataUpdate, updateInterval)
+}
+
+function memoryResourceUpdate() {
+    memory_chart.setData([getRandomData()])
+    memory_chart.draw()
+    setTimeout(memoryResourceUpdate, updateInterval)
+}
+
+// // about performance
+processingSpeedUpdate()
+processingDataUpdate()
+memoryResourceUpdate()
 
 
-/*
- * Custom Label formatter
- * ----------------------
- */
 function labelFormatter(label, series) {
     return '<div style="font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;">'
         + label
@@ -177,6 +188,48 @@ function labelFormatter(label, series) {
         + Math.round(series.percent) + '%</div>'
 }
 
-function clickedToggle(toggle) {
-    $("#chart2").load(window.location.href+"#chart3");
-}
+// function clickedMenu(menu) {
+//     var chart = menu.hash;
+//     var newChart = $(chart).parents("chart1").find(".drop").attr('id', chart).getAttribute("id");
+//
+//     alert(newChart);
+//     $(newChart).empty();
+//
+//     // if (chart == "#disk-chart") {
+//     //     $(chart).empty();
+//     // }
+//     // else {
+//     //     $("#processingData-chart").empty();
+//     //     var processingData_chart = $.plot('#processingData-chart', [
+//     //             {
+//     //                 data: getRandomData(),
+//     //             }
+//     //         ],
+//     //         {
+//     //             grid: {
+//     //                 borderColor: '#f3f3f3',
+//     //                 borderWidth: 1,
+//     //                 tickColor: '#f3f3f3'
+//     //             },
+//     //             series: {
+//     //                 color: '#3c8dbc',
+//     //                 lines: {
+//     //                     lineWidth: 2,
+//     //                     show: true,
+//     //                     fill: true,
+//     //                 },
+//     //             },
+//     //             yaxis: {
+//     //                 min: 0,
+//     //                 max: 100,
+//     //                 show: true
+//     //             },
+//     //             xaxis: {
+//     //                 show: true
+//     //             }
+//     //         }
+//     //     )
+//     //     processingDataUpdate()
+//     // }
+// }
+
